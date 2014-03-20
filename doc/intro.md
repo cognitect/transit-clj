8c8f765d975aca127865c2a53d691fb9159b3e32
Tim Ewald <tim.ewald@gmail.com>
2014-03-20 11:11:59 -0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to transit-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/transit_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns transit-clj.core-test
+  (:require [clojure.test :refer :all]
+            [transit-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
