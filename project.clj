b7104be6150da67c77c7cf1266570fb16fc691ba
Tim Ewald <tim.ewald@gmail.com>
2014-03-25 15:21:50 -0400

    Fix typo in project file

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject transit-clj "0.1.0-SNAPSHOT"
+(defproject transit-clj "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :dependencies [[com.fasterxml.jackson.core/jackson-core "2.3.1"]
                  [org.msgpack/msgpack "0.6.9"]
                  [org.clojure/data.fressian "0.2.0"]
-                 [commons-codec 1.5]]
+                 [commons-codec "1.5"]]
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :dev {:dependencies [[org.clojure/data.generators "0.1.2"]]}})
 
