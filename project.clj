bad47b237300c097f238eb4ed00efc086e806add
Tim Ewald <tim.ewald@gmail.com>
2014-04-17 11:41:02 -0400

    Switch to SNAPSHOT version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject transit-clj "0.1.0"
+(defproject transit-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
